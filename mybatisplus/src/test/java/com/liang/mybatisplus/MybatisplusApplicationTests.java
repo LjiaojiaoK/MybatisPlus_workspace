1ebd9b560e099183700197fd5cdcd06a067fe082
liang <liang@qq.com>
2022-04-26 20:52:06 +0800

    push commit

--- mybatisplus/src/test/java/com/liang/mybatisplus/MybatisplusApplicationTests.java
+++ mybatisplus/src/test/java/com/liang/mybatisplus/MybatisplusApplicationTests.java
@@ -12,7 +12,7 @@ class MybatisplusApplicationTests {
         System.out.println("hot-fix");
         System.out.println("master test");
         System.out.println("hot-test");
-
+        System.out.println("push test");
     }
 
 }
